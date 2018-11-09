7143af4b68377099b2702933e6ebcc512284a093
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-09 18:51:00 +0700

    bind on construct

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from './Header';
 
 class App extends Component {
-  render() {
-    let sample = "sample 01";
-    return (
-      <div className="App">
-          <Header
-          title={sample}
-          counter="01"
-          />
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            likeMe:1
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    render() {
+        let sample = "sample 01";
+        return (
+            <div className="App">
+                <Header
+                    title={sample}
+                    counter="01"
+                />
+
+                {/*<button onClick={this.handleClick.bind(this)}>Click Me</button>*/}
+                {/*<button onClick={()=>this.handleClick()}>Click Me</button>*/}
+                <button onClick={this.handleClick}>Click Me</button>
+            </div>
+        );
+    }
+
+    handleClick() {
+        console.log(this.state.likeMe)
+    }
 }
 
 export default App;
